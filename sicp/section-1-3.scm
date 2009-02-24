44bb893fbb75c609dd2c70ce0369e9cb27388cfa
James Coglan <jcoglan@googlemail.com>
2009-02-24 21:37:55 +0000

    Solution to SICP 1.35

--- sicp/section-1-3.scm
+++ sicp/section-1-3.scm
@@ -159,3 +159,45 @@
 
 ; (f f) -> (f 2) -> (2 2) -> invalid expression
 
+
+(exercise "1.35")
+; Golden ratio as a fixed point
+
+(define (search f neg-point pos-point)
+  (let ((midpoint (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+        midpoint
+        (let ((test-value (f midpoint)))
+          (cond ((positive? test-value)
+                 (search f neg-point midpoint))
+                ((negative? test-value)
+                 (search f midpoint pos-point))
+                (else midpoint))))))
+
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.001))
+
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+        (b-value (f b)))
+    (cond ((and (negative? a-value) (positive? b-value))
+           (search f a b))
+          ((and (negative? b-value) (positive? a-value))
+           (search f b a))
+          (else
+           (error "Values are not of opposite sign" a b)))))
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(output "(fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0)")
+; 1.61803278688525
+
