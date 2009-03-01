6c4eae182efd49eba1c727d00334c899f3716d1d
James Coglan <jcoglan@googlemail.com>
2009-03-01 14:06:58 +0000

    Move macro expansion logic into the Macro::Expansion class. Removes some of the statefulness from Macro and clarifies some of the hygiene logic.

--- lib/runtime/callable/macro.rb
+++ lib/runtime/callable/macro.rb
@@ -10,16 +10,10 @@ module Heist
         require RUNTIME_PATH + 'callable/macro/' + klass
       end
       
-      def initialize(scope, *args)
-        super
-        @hygienic = scope.runtime.hygienic?
-      end
-      
       def call(scope, cells)
-        @calling_scope = scope
         rule, matches = *rule_for(cells)
         
-        return Expansion.new(expand_template(rule.cdr.car, matches)) if rule
+        return Expansion.new(@scope, scope, rule.cdr.car, matches) if rule
         
         input = cells.map { |c| c.to_s } * ' '
         raise SyntaxError.new(
@@ -104,80 +98,6 @@ module Heist
             return pattern == input ? true : nil
         end
         matches
-      end
-      
-      # When a macro use is transcribed according to the template of the
-      # matching <syntax rule>, pattern variables that occur in the template
-      # are replaced by the subforms they match in the input. Pattern variables
-      # that occur in subpatterns followed by one or more instances of the
-      # identifier '...' are allowed only in subtemplates that are followed
-      # by as many instances of '...'. They are replaced in the output by all
-      # of the subforms they match in the input, distributed as indicated. It
-      # is an error if the output cannot be built up as specified.
-      # 
-      # Identifiers that appear in the template but are not pattern variables
-      # or the identifier '...' are inserted into the output as literal
-      # identifiers. If a literal identifier is inserted as a free identifier
-      # then it refers to the binding of that identifier within whose scope
-      # the instance of 'syntax-rules' appears. If a literal identifier is
-      # inserted as a bound identifier then it is in effect renamed to prevent
-      # inadvertent captures of free identifiers.
-      # 
-      def expand_template(template, matches, depth = 0, inspection = false)
-        case template
-        
-          when Cons then
-            result, last, repeater, template_pair = nil, nil, nil, template
-            
-            push = lambda do |value|
-              return if value == Cons::NULL
-              pair = Cons.new(value)
-              result ||= pair
-              last.cdr = pair if last
-              last = pair
-            end
-            
-            while not template_pair.null?
-              cell = template_pair.car
-              followed_by_ellipsis = (template_pair.cdr.car.to_s == ELLIPSIS)
-              dx = followed_by_ellipsis ? 1 : 0
-              
-              matches.inspecting(depth + 1) if followed_by_ellipsis and
-                                               not inspection
-              
-              repeater = cell if followed_by_ellipsis
-              
-              if cell.to_s == ELLIPSIS and not inspection
-                matches.expand! { push[expand_template(repeater, matches, depth + 1)] }
-                matches.depth = depth
-              else
-                inspect = inspection || (followed_by_ellipsis && depth + 1)
-                value = expand_template(cell, matches, depth + dx, inspect)
-                push[value] unless inspect
-              end
-              
-              template_pair = template_pair.cdr
-            end
-            result
-        
-          when Identifier then
-            return matches.get(template) if matches.defined?(template)
-            return Identifier.new(template) unless @hygienic
-            
-            @scope.defined?(template) ?
-                Binding.new(template, @scope, false) :
-                rename(template)
-        
-          else
-            template
-        end
-      end
-      
-      def rename(id)
-        return id unless @calling_scope.defined?(id)
-        i = 1
-        i += 1 while @calling_scope.defined?("#{id}#{i}")
-        Identifier.new("#{id}#{i}")
       end
     end
     
--- lib/runtime/callable/macro/expansion.rb
+++ lib/runtime/callable/macro/expansion.rb
@@ -4,8 +4,88 @@ module Heist
       
       class Expansion
         attr_reader :expression
-        def initialize(expression)
-          @expression = expression
+        
+        def initialize(lexical_scope, calling_scope, template, matches)
+          @lexical_scope = lexical_scope
+          @calling_scope = calling_scope
+          @hygienic      = lexical_scope.runtime.hygienic?
+          @expression    = expand(template, matches)
+        end
+        
+      private
+        
+        # When a macro use is transcribed according to the template of the
+        # matching <syntax rule>, pattern variables that occur in the template
+        # are replaced by the subforms they match in the input. Pattern variables
+        # that occur in subpatterns followed by one or more instances of the
+        # identifier '...' are allowed only in subtemplates that are followed
+        # by as many instances of '...'. They are replaced in the output by all
+        # of the subforms they match in the input, distributed as indicated. It
+        # is an error if the output cannot be built up as specified.
+        # 
+        # Identifiers that appear in the template but are not pattern variables
+        # or the identifier '...' are inserted into the output as literal
+        # identifiers. If a literal identifier is inserted as a free identifier
+        # then it refers to the binding of that identifier within whose scope
+        # the instance of 'syntax-rules' appears. If a literal identifier is
+        # inserted as a bound identifier then it is in effect renamed to prevent
+        # inadvertent captures of free identifiers.
+        # 
+        def expand(template, matches, depth = 0, inspection = false)
+          case template
+          
+            when Cons then
+              result, last, repeater, template_pair = nil, nil, nil, template
+              
+              push = lambda do |value|
+                return if value == Cons::NULL
+                pair = Cons.new(value)
+                result ||= pair
+                last.cdr = pair if last
+                last = pair
+              end
+              
+              while not template_pair.null?
+                cell = template_pair.car
+                followed_by_ellipsis = (template_pair.cdr.car.to_s == ELLIPSIS)
+                dx = followed_by_ellipsis ? 1 : 0
+                
+                matches.inspecting(depth + 1) if followed_by_ellipsis and
+                                                 not inspection
+                
+                repeater = cell if followed_by_ellipsis
+                
+                if cell.to_s == ELLIPSIS and not inspection
+                  matches.expand! { push[expand(repeater, matches, depth + 1)] }
+                  matches.depth = depth
+                else
+                  inspect = inspection || (followed_by_ellipsis && depth + 1)
+                  value = expand(cell, matches, depth + dx, inspect)
+                  push[value] unless inspect
+                end
+                
+                template_pair = template_pair.cdr
+              end
+              result
+          
+            when Identifier then
+              return matches.get(template) if matches.defined?(template)
+              return Identifier.new(template) unless @hygienic
+              
+              @lexical_scope.defined?(template) ?
+                  Binding.new(template, @lexical_scope, false) :
+                  rename(template)
+          
+            else
+              template
+          end
+        end
+        
+        def rename(id)
+          return id unless @calling_scope.defined?(id)
+          i = 1
+          i += 1 while @calling_scope.defined?("#{id}#{i}")
+          Identifier.new("#{id}#{i}")
         end
       end
       
