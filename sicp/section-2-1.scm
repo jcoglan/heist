67b2a3dfd60b5a587ed8ff2723c43febf2dfdf5b
James Coglan <jcoglan@googlemail.com>
2009-03-04 14:02:25 +0000

    Solutions for SICP exercises 2.7 to 2.10

--- sicp/section-2-1.scm
+++ sicp/section-2-1.scm
@@ -197,3 +197,79 @@
 (define three (add one (add (add one zero) one)))
 (output "((three inc) 0)")
 
+
+; Functions for extended exercise
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (mul-interval x 
+                (make-interval (/ 1.0 (upper-bound y))
+                               (/ 1.0 (lower-bound y)))))
+
+
+(exercise "2.7")
+; Interval constructors
+; upper-bound and lower-bound ought to return the
+; greater and lesser bounds, respectively
+
+(define (make-interval a b) (cons a b))
+
+(define (upper-bound int)
+  (max (car int) (cdr int)))
+
+(define (lower-bound int)
+  (min (car int) (cdr int)))
+
+
+(exercise "2.8")
+; Interval subtraction
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
+
+
+(exercise "2.9")
+; Widths of intervals
+
+(define (width-interval int)
+  (/ (- (upper-bound int)
+        (lower-bound int))
+     2))
+
+; Expand (w (+ x y)):
+; w = width, i = make, l = lower, u = upper
+; 
+; (w (i (+ (l x) (l y)) (+ (u x) (u y))))
+; (/ (- (+ (u x) (u y)) (+ (l x) (l y))) 2)
+; 
+; Which is equal to
+; (+ (/ (- (u x) (l x)) 2) (/ (- (u y) (l y)) 2))
+; = (+ (w x) (w y))
+
+
+(exercise "2.10")
+; Check for zero-spanning intervals
+
+(define (spans-zero? int)
+  (and (negative? (lower-bound int))
+       (not (negative? (upper-bound int)))))
+
+(define (div-interval x y)
+  (if (spans-zero? y)
+      (error "Cannot divide by an interval that spans zero")
+      (mul-interval x 
+                    (make-interval (/ 1.0 (upper-bound y))
+                                   (/ 1.0 (lower-bound y))))))
+
