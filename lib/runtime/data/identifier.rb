21f4259d464b1eeb682f342190f00aae8f4af0d7
James Coglan <jcoglan@googlemail.com>
2009-03-19 14:19:18 +0000

    Remove unused metadata code from Identifier. I tried using it to cache macro variable lists on syntax objects and it didn't really make a difference.

--- lib/runtime/data/identifier.rb
+++ lib/runtime/data/identifier.rb
@@ -5,13 +5,11 @@ module Heist
       include Expression
       
       extend Forwardable
-      def_delegators(:@metadata, :[], :[]=)
       def_delegators(:@name, :to_s)
       alias :inspect :to_s
       
       def initialize(name)
         @name = name.to_s
-        @metadata = {}
       end
       
       def ==(other)
