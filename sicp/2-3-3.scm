115ee4ac11e428b6af9027891553aac9136e3190
James Coglan <jcoglan@googlemail.com>
2009-08-27 13:42:38 +0100

    Fixing buggy solution to SICP 2.62.

--- sicp/2-3-3.scm
+++ sicp/2-3-3.scm
@@ -100,12 +100,16 @@
 (define (union-set set1 set2)
   (cond ((null? set1) set2)
         ((null? set2) set1)
-        ((< (car set1) (car set2))
-          (cons (car set1)
-                (union-set (cdr set1) set2)))
-        (else set2)))
+        (else
+          (let ((x1 (car set1)) (x2 (car set2)))
+            (cond ((= x1 x2)
+                   (cons x1 (union-set (cdr set1) (cdr set2))))
+                  ((< x1 x2)
+                   (cons x1 (union-set (cdr set1) set2)))
+                  ((> x1 x2)
+                   (cons x2 (union-set set1 (cdr set2)))))))))
 
-(output '(union-set '(1 2 3 6 8) '(3 5 6 8 9)))
+(output '(union-set '(1 2 3 6 8 11) '(3 5 6 8 9)))
 
 
 ; Binary tree representation
